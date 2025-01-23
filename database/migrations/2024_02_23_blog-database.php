1bbac500b82d087db0a6a2c079e82bbcff28fbcf
sahrulprograming <sahrulsaefulah@gmail.com>
2025-01-23 13:50:43 +0700

    test

--- database/migrations/2024_02_23_blog-database.php
+++ database/migrations/2024_02_23_blog-database.php
@@ -4,9 +4,12 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateBlogsTable extends Migration
+return new class extends Migration
 {
-    public function up()
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
     {
         Schema::create('blogs', function (Blueprint $table) {
             $table->id();
@@ -21,8 +24,11 @@ class CreateBlogsTable extends Migration
         });
     }
 
-    public function down()
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
     {
         Schema::dropIfExists('blogs');
     }
-}
+};
